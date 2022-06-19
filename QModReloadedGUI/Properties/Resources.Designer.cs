d466462a36eb6549424ca8c1f3dbdc52ac66371a
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2022-06-19 12:41:08 +1000

    updated about page logo

--- QModReloadedGUI/Properties/Resources.Designer.cs
+++ QModReloadedGUI/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace QModReloadedGUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Color_logo___no_background {
+            get {
+                object obj = ResourceManager.GetObject("Color logo - no background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap comments {
             get {
                 object obj = ResourceManager.GetObject("comments", resourceCulture);
